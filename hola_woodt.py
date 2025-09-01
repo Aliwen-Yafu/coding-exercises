db39e0a04c4399dd0c948a8bd68f24c4bcc85ea2
Aliwen Delgado <68169695+Aliwen-Yafu@users.noreply.github.com>
2025-09-01 19:17:47 +0000

    agregar archivo inicial de prueba

---
+++ hola_woodt.py
@@ -0,0 +1,7 @@
+
+def saludar(nombre):
+    return f"Hola {nombre}, listo para la entrevista!"
+
+if __name__ == "__main__":
+    print(saludar("WoodT"))
+    print("Python funcionando correctamente")
